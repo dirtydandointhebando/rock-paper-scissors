caff039b0331f9ba4f61bdaba17b20e982129aa4
Aidan <aidandepaul@gmail.com>
2025-03-19 20:42:49 -0400

    Store userChoice as a prompt

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -5,7 +5,7 @@
 
 */
 
-// use the Math.random function to assign a value from 1-3 and then assign rock, paper, or scissors based on that value
+// Use the Math.random function to assign a value from 1-3 and then assign rock, paper, or scissors based on that value
 function getComputerChoice() {
     const number = Math.floor(Math.random() * 3 + 1);
     if (number === 1) return "Rock";
@@ -13,5 +13,21 @@ function getComputerChoice() {
     else if (number === 3) return "Scissors"; 
 }
 
+// Assign getComputerChoice() value to variable computerChoice
 let computerChoice = (getComputerChoice());
 console.log(computerChoice);
+
+// Use a prompt to get the user's choice 
+
+const userChoice = prompt("Ready to play? Rock... paper... scissors!")
+console.log(userChoice);
+
+/*
+function winner(userChoice) {
+    if (userChoice === "Rock" && computerChoice === "Scissors") {
+        alert("Rock beats scissors! You win!");
+    }
+}
+
+winner();
+*/
