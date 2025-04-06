dccb2d5ac870af60d9abf011858ccaf16c05ac66
Aidan <aidandepaul@gmail.com>
2025-04-06 11:15:47 -0400

    Add variables to log score for human & comp scores

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,3 +1,10 @@
+// Requirements
+// 1. Assign rpc choice to computer using Math.random (done)
+// 2. Use a prompt for user input (replace the prompt with buttons later)
+// 3. Create a function that uses conditions play one round of rock paper scissors, log score of winner
+// 4. Create a function that plays five rounds and determines a winner
+
+
 function getComputerChoice() {
     const number = Math.floor(Math.random() * 3 + 1);
     if (number === 1) return "rock";
@@ -6,3 +13,6 @@ function getComputerChoice() {
 }
 
 console.log(getComputerChoice());
+
+let humanScore = 0;
+let computerScore = 0;
