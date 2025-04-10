e396668c62a2bc6a67b91f5a282b4d1ffaf3dd90
Aidan <aidandepaul@gmail.com>
2025-04-09 23:29:55 -0400

    move commented out js code to bottom of doc

--- staging.js
+++ staging.js
@@ -12,6 +12,55 @@ const output = document.querySelector("#output");
 const choices = document.createElement("p");
 const outcome = document.createElement("p");
 
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        choices.textContent = `Both players pick ${humanChoice}.`;
+        output.appendChild(choices);
+        outcome = `It's a draw! The score is still ${humanScore} for the human and ${computerScore} for the computer!`;
+        output.appendChild(outcome);
+    } else if (humanChoice === "rock" && computerChoice === "scissors") {
+        choices.textContent = "Rock beats scissors! Meatbag wins!";
+        output.appendChild(choices);
+        humanScore++;
+        outcome = `Human score is now ${humanScore}. The computer's score is ${computerScore}.`;
+        output.appendChild(outcome);
+    } else if (humanChoice === "rock" && computerChoice === "paper") {
+        choices.textContent = "Paper beats rock. The machine wins!";
+        output.appendChild(choices);
+        computerScore++
+        outcome = `Human score is now ${humanScore}. The computer's score is ${computerScore}.`;
+        output.appendChild(outcome);
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+        choices.textContent = "Paper beats rock! Meatbag wins!";
+        output.appendChild(choices);
+        humanScore++;
+        outcome = `Human score is now ${humanScore}. The computer's score is ${computerScore}.`;
+        output.appendChild(outcome);
+    } else if (humanChoice === "paper" && computerChoice === "scissors") {
+        choices.textContent = "Scissors beats paper! Machine wins!";
+        output.appendChild(choices);
+        computerScore++;
+        outcome = `Human score is now ${humanScore}. The computer's score is ${computerScore}.`;
+        output.appendChild(outcome);
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+        choices.textContent = "Scissors beats paper! Meatbag wins! What a play!";
+        output.appendChild(choices);
+        humanScore++;
+        outcome = `Human score is now ${humanScore}. The computer's score is ${computerScore}.`;
+        output.appendChild(outcome);
+    } else if (humanChoice === "scissors" && computerChoice === "rock") {
+        choices.textContent = "Rock beats scissors! Meatbag loses.";
+        output.appendChild(choices);
+        computerScore++;
+        outcome = `Human score is now ${humanScore}. The computer's score is ${computerScore}.`;
+        output.appendChild(outcome);
+    }
+}
+
+
+
+
+
 /*
 function playRound(humanChoice, computerChoice) {
     if (humanChoice === computerChoice) {
@@ -66,48 +115,3 @@ function playGame() {
 
 playGame();
 */
-
-function playRound(humanChoice, computerChoice) {
-    if (humanChoice === computerChoice) {
-        choices.textContent = `Both players pick ${humanChoice}.`;
-        output.appendChild(choices);
-        outcome = `It's a draw! The score is still ${humanScore} for the human and ${computerScore} for the computer!`;
-        output.appendChild(outcome);
-    } else if (humanChoice === "rock" && computerChoice === "scissors") {
-        choices.textContent = "Rock beats scissors! Meatbag wins!";
-        output.appendChild(choices);
-        humanScore++;
-        outcome = `Human score is now ${humanScore}. The computer's score is ${computerScore}.`;
-        output.appendChild(outcome);
-    } else if (humanChoice === "rock" && computerChoice === "paper") {
-        choices.textContent = "Paper beats rock. The machine wins!";
-        output.appendChild(choices);
-        computerScore++
-        outcome = `Human score is now ${humanScore}. The computer's score is ${computerScore}.`;
-        output.appendChild(outcome);
-    } else if (humanChoice === "paper" && computerChoice === "rock") {
-        choices.textContent = "Paper beats rock! Meatbag wins!";
-        output.appendChild(choices);
-        humanScore++;
-        outcome = `Human score is now ${humanScore}. The computer's score is ${computerScore}.`;
-        output.appendChild(outcome);
-    } else if (humanChoice === "paper" && computerChoice === "scissors") {
-        choices.textContent = "Scissors beats paper! Machine wins!";
-        output.appendChild(choices);
-        computerScore++;
-        outcome = `Human score is now ${humanScore}. The computer's score is ${computerScore}.`;
-        output.appendChild(outcome);
-    } else if (humanChoice === "scissors" && computerChoice === "paper") {
-        choices.textContent = "Scissors beats paper! Meatbag wins! What a play!";
-        output.appendChild(choices);
-        humanScore++;
-        outcome = `Human score is now ${humanScore}. The computer's score is ${computerScore}.`;
-        output.appendChild(outcome);
-    } else if (humanChoice === "scissors" && computerChoice === "rock") {
-        choices.textContent = "Rock beats scissors! Meatbag loses.";
-        output.appendChild(choices);
-        computerScore++;
-        outcome = `Human score is now ${humanScore}. The computer's score is ${computerScore}.`;
-        output.appendChild(outcome);
-    }
-}
